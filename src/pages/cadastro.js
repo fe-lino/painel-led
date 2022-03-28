e3890e71b03ef6c5627657a0ec9aea96cb2d8408
Felipe Santos Lino <43026998842@info.scs>
2022-03-28 11:15:27 -0300

    teste de function

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from '../../src/assets/img/logoCadastro.png'
 import logoSenai from '../../src/assets/img/logoSenai.svg'
 import '../../src/styles/cadastro.css'
 
 export default function Cadastro() {
+
+    
+
     
     return(
         <div className='main'>
             <div className="ladoB">
-                <img className='logo' src={logo}></img>
+                <img className='logo' src={logo} alt='Logo Senai'></img>
             </div>
 
             <div className="ladoV">
@@ -17,21 +20,38 @@ export default function Cadastro() {
 
                     <form className='formulario'>
                         <label>Nome</label>
-                        <input type='text'></input>
+                        <input 
+                        type='text' 
+                        name='nomeUsuario' 
+                  
+                    
+                        ></input>
 
                         <label>Email</label>
-                        <input type='email'></input>
+                        <input 
+                        type='email' 
+                        nome='email'
+                
+         
+                        ></input>
 
                         <label>Senha</label>
-                        <input type='password'></input>
+                        <input
+                        type='password'
+                        name='senha'
+             
+           
+                        ></input>
 
                         <div className='divBotao'>
-                        <button type='submit'>Conectar</button>
+                        <button 
+                        type='button'>Cadastrar</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     );
+
     
 };
